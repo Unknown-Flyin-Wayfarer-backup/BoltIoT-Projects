02ac6a96c0fa09a7daa13a18368e865c0d871274
Unknown-Flyin-Wayfarer-backup <82906990+Unknown-Flyin-Wayfarer-backup@users.noreply.github.com>
2021-06-25 10:25:53 +0530

    Add files via upload

---
+++ email_conf.py
@@ -0,0 +1,6 @@
+MAILGUN_API_KEY = 'My MailGun API Key' 
+SANDBOX_URL= 'my SandNox URL here' 
+SENDER_EMAIL = 'test@' + SANDBOX_URL  # No need to modify this. The sandbox URL is of the format test@YOUR_SANDBOX_URL
+RECIPIENT_EMAIL = 'my Email ID Here'
+API_KEY = 'API key of my Bolt IoT device'
+DEVICE_ID = 'ID of my Bolt device' 
---
+++ temp_alert_telegram.py
@@ -0,0 +1,5 @@
+bolt_api_key = "XXXX"                 # This is my Bolt Cloud API Key
+device_id = "XXXX"                    # This is the device ID
+telegram_chat_id = "@XXXX"            # This is the channel ID of the created Telegram channel
+telegram_bot_id = "botXXXX"           # This is the bot ID of the created Telegram Bot. Paste after bot
+threshold = 250                       # Threshold beyond which the alert should be sent
